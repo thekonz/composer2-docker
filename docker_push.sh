68e15d94f59b9e925439b970b82ca20a4d9005da
Konstantin Duczek <konz.duczek@gmail.com>
2020-04-23 19:18:51 +0200

    fix invalid tag chars

--- docker_push.sh
+++ docker_push.sh
@@ -6,7 +6,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build -t tempname .
 
-VERSION=$(docker run tempname --version | egrep -o '([0-9]+\.?){2}-dev\+.{7}' | head -1)
+VERSION=$(docker run tempname --version | egrep -o '([0-9]+\.?){2}-dev\+.{7}' | head -1 | sed s/dev+// -)
 echo "Built version $VERSION"
 docker tag tempname thekonz/composer2:$VERSION
 docker tag tempname thekonz/composer2:latest
